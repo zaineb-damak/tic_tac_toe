afbf944ddf6f84aa80886ac43d61ed59acc7470c
zaineb-damak <77733791+zaineb-damak@users.noreply.github.com>
2023-11-25 13:33:11 +0100

    second version without checking win

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,80 +1,80 @@
-import tkinter as tk
-from tkinter import font
-from typing import NamedTuple
-from itertools import cycle
+import random
 
-class TikTacToeBoard(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Tic Tac Toe")
-        self._cells = {}
-        self._create_board_display()
-        self._create_board_grid()
+board = [" " for i in range(9)]
 
-    def _create_board_display(self):
-        display_frame = tk.Frame(master=self)
-        display_frame.pack(fill=tk.X)
-        self.display=tk.Label(
-            master=display_frame,
-            text="Ready?"
-        )
-        self.display.pack()
+#turn =random.randint(0,1) # 0 for player, 1 for computer
+player_turn = 0
+turn_in_game = 0
 
-    def _create_board_grid(self):
-        grid_frame = tk.Frame(master=self)
-        grid_frame.pack()
-        for row in range(3):   
-            self.rowconfigure(row, weight=1, minsize=50)
-            self.rowconfigure(row, weight=1, minsize=75)
-            for col in range(3):
-                button = tk.Button(
-                    master=grid_frame,
-                    text="",
-                    width=3,
-                    height=2
-                )
-                self._cells[button]=(row,col)
-                button.grid(
-                    row=row,
-                    column=col,
-                    padx=5,
-                    pady=5,
-                    sticky="nsew"
-                )
+player = "X"
+computer = "O"
 
-class Player(NamedTuple):
-     label: str
-     color: str
+winning_combo = [  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], ]
 
-class Move(NamedTuple):
-     row: int
-     col: int
-     label: str     
+def draw_board():
+    for row in range(3):
+        print("|".join(board[row * 3:row * 3 + 3]))
+        
+        
+def game_play(player_turn, turn_in_game):
+    while True:
+        if turn_in_game > 8 :
+            print("it's a tie")
+            break
 
-class TicTacToeGame:
-    BOARD_SIZE=3
+        draw_board()
+        if player_turn== 1:
+            print("computer's move")
+            board[computer_move()]=computer
+            # if check_win(computer):
+            #     print('computer won')
+            #     break
+            player_turn=0
+            turn_in_game += 1
+            continue
+        else:
+            try:
+                move = int(input("enter a number from 1 to 9 \n")) - 1
+            except ValueError:
+                # Not an integer
+                print('That\'s not a valid number, try again.\n')
+                continue
+            if move<0 or move>8:
+                print("number out of range. try again")
+                continue
+            if board[move] == " ":
+                board[move]=player
+                # if check_win(player):
+                #     print("you won")
+                #     break
+                player_turn=1
+                turn_in_game += 1
+                continue
+            else:
+                print("that box is already marked. try again")
+
+def computer_move():
+    available_moves=[i for i in range(9) if board[i]==" "]
+    if len(available_moves)>0:
+        return random.choice(available_moves)
+
+def check_win(player_symbol):
+    score=0
+    for combo in winning_combo:
+        print(combo)
+        for index in combo:
+            if board[index] == player_symbol:
+                score += 1
+                print(score)
+            else:
+                break
+            if score == 3:
+                return True
     
-    DEFAULT_PLAYERS = (
-        Player(label="X", color="blue"),
-        Player(label="O", color="green")
-    )
-    def __init__(self, players= DEFAULT_PLAYERS, board_size=BOARD_SIZE):
-          self._players= cycle(players)
-          self.current_player = next(self._players)
-          self.winner_combo = []
-          self._current_moves = []
-          self._has_winner = False
-          self._winning_combos = []
-          self._setup_board()
 
-    def _setup_board():
-         return
-    
-    
-            
-def main():
-        board = TikTacToeBoard()
-        board.mainloop()
+if __name__ == "__main__":
+   game_play(player_turn, turn_in_game)
+  
 
-if __name__ =="__main__":
-        main()
+    
